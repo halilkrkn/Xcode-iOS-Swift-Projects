0ccc3619e4059e884149cb95285495204a28d320
Halil Karkın <Halilkrkn000@gmail.com>
2022-03-12 14:42:24 +0300

    First iOS Swift App - Hi, Halilkrkn

---
+++ FirstIOSAppWithSwift/FirstIOSAppWithSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstIOSAppWithSwift
+//
+//  Created by karkinh on 12.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

76baf72dde1477225c8e8d28209face718a0c6e8
Halil Karkın <Halilkrkn000@gmail.com>
2022-03-28 12:57:59 +0300

    Betwen Screens Data Transfer with Segue and Identifier

---
+++ iOS-Programming-with-Swift/MovingDataBetweenScreens/MovingDataBetweenScreens/VC_Green.swift
@@ -0,0 +1,30 @@
+//
+//  VC_Green.swift
+//  MovingDataBetweenScreens
+//
+//  Created by karkinh on 28.03.2022.
+//
+
+import UIKit
+
+class VC_Green: UIViewController {
+
+    var gelen: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

f4a1f132ffed506791a65d241b0afcd4a43a6c2d
Halil Karkın <Halilkrkn000@gmail.com>
2022-03-28 16:59:07 +0300

    back and forth movement and restrictions with navigation controller
    Navigation Controllerları arasından ileri geri ekranlara geçişi ve Ekranlar arasındaki Kısıtlamalar Yapıldı.

---
+++ iOS-Programming-with-Swift/NavigationControllerLAB/NavigationControllerLAB/VCSignUp.swift
@@ -0,0 +1,29 @@
+//
+//  VCSignUp.swift
+//  NavigationControllerLAB
+//
+//  Created by karkinh on 28.03.2022.
+//
+
+import UIKit
+
+class VCSignUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

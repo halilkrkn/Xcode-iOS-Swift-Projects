b92fe23f7406e38adcd72de68a71566b2b9e3051
Halil Karkın <Halilkrkn000@gmail.com>
2022-04-03 13:21:04 +0300

    TableViewCustom Desing
    - TableviewController ile özel tasarımlarla verileri listeleme anlatılıyor.
    - Bu projede kendimiz Tableview için satırlar(row) oluşturduk ve TableViewdaki listeleme görünümü sağlattık.

---
+++ iOS-Programming-with-Swift/Listing/TableViewCustomDesing/TableViewCustomDesing/TableViewCell_Satir.swift
@@ -0,0 +1,22 @@
+////
+//  TableViewCustomDesing
+//  Created by halilkrkn on 3.04.2022
+//
+
+import UIKit
+
+class TableViewCell_Satir: UITableViewCell {
+
+    @IBOutlet weak var lblMetin: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iOS-Programming-with-Swift/Listing/TableViewCustomDesing/TableViewCustomDesing/ViewController.swift
@@ -0,0 +1,17 @@
+////
+//  TableViewCustomDesing
+//  Created by halilkrkn on 3.04.2022
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

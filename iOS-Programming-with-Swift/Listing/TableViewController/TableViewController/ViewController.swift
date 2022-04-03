cf8a9ac380518b517d8aa5cb5bbf08e8376db00a
Halil Karkın <Halilkrkn000@gmail.com>
2022-04-03 12:14:49 +0300

    TabelViewController - Standart Cell Learned.
    - Burada TableViewController yapısı öğrenildi.
    -TableviewController ile standart veri tasarımında veriler listeleme anlatılıyor.

---
+++ iOS-Programming-with-Swift/Listing/TableViewController/TableViewController/ViewController.swift
@@ -0,0 +1,21 @@
+////
+//  TableViewController
+//  Created by halilkrkn on 3.04.2022
+//
+
+// TableView:
+// Liste olarak birden fazla veriyi tutmak için kullanılır.
+// TVCListe Sınıfında TableView Kodları Mevcut.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

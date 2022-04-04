962ce854ed663fbfe00fdc19a5b4ff1ba9b005d9
Halil Karkın <Halilkrkn000@gmail.com>
2022-04-04 10:57:56 +0300

    CollectionView Learned
    - CollectionView Yapısı Öğrenildi.
    - Verileri birden fazla sütun, scroll edilebilir, oval gibi farklı hallerde listeleme görünümüne sahip
     olmamıza olanak sağlar.
    - CollectionView Android'deki RecyclerView Yapısı gibidir.

---
+++ iOS-Programming-with-Swift/Listing/CollectionView/CollectionView/CVC_Item.swift
@@ -0,0 +1,16 @@
+////
+//  CollectionView
+//  Created by halilkrkn on 4.04.2022
+//
+
+import UIKit
+
+class CVC_Item: UICollectionViewCell {
+
+    @IBOutlet weak var lblMetin: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

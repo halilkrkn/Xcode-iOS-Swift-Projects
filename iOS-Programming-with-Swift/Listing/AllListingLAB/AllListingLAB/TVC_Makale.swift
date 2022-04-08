1a30f145fc314e2eee72b81e950332e4595b5d52
Halil İbrahim KARKIN <Halilkrkn000@gmail.com>
2022-04-08 11:37:30 +0300

    Finished with ListingLAB
    - Listeleme için kullanılan tüm öğrenilen method yapıları bu projede kullanıldı.

---
+++ iOS-Programming-with-Swift/Listing/AllListingLAB/AllListingLAB/TVC_Makale.swift
@@ -0,0 +1,24 @@
+////
+//  AllListingLAB
+//  Created by halilkrkn on 5.04.2022
+//
+
+import UIKit
+
+class TVC_Makale: UITableViewCell {
+
+    @IBOutlet weak var lblBaslik: UILabel!
+    @IBOutlet weak var lblKisaAciklama: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
